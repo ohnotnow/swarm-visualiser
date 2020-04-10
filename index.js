7f74813d7731acd45450a50bcf0e4991dd0c9324
Billy <billybofh@gmail.com>
2020-04-10 11:08:47 +0100

    Some tidying up

--- index.js
+++ index.js
@@ -1,33 +1,39 @@
-const Docker = require('dockerode');
-const express = require('express');
-const cors = require('cors');
-const path = require('path');
+const Docker = require("dockerode");
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
 
 const app = express();
 app.use(cors());
-const port = 3000;
-var docker = new Docker({socketPath: '/var/run/docker.sock'});
+const docker = new Docker({ socketPath: "/var/run/docker.sock" });
 
+const port = process.env.VIS_PORT ? process.env.VIS_PORT : 3000;
+const refreshInterval = process.env.VIS_REFRESH ? process.env.VIS_REFRESH : 1000;
 var nodeList = [];
 var taskList = [];
 
-async function init() {
-    nodeList = await docker.listNodes();
-    taskList = await docker.listTasks();
-    setTimeout(init, 1000);
+async function refreshDockerInfo() {
+  nodeList = await docker.listNodes().catch(err => console.error(err));
+  taskList = await docker.listTasks().catch(err => console.error(err));
+  setTimeout(refreshDockerInfo, refreshInterval);
 }
-docker.listNodes().then(nodes => nodeList = nodes);
 
-init();
+refreshDockerInfo();
 
-app.use(express.static('public'))
+app.use(express.static("public"));
 
-app.get('/', (req, res) => res.sendFile('index.html', {root: path.join(__dirname, 'public')}, err => {
-    console.error(err);
-}));
-app.get('/tasks', (req, res) => res.send(taskList));
-app.get('/nodes', (req, res) => res.send(nodeList));
+app.get("/", (req, res) =>
+  res.sendFile(
+    "index.html",
+    { root: path.join(__dirname, "public") },
+    (err) => {
+      console.error(err);
+    }
+  )
+);
+app.get("/tasks", (req, res) => res.send(taskList));
+app.get("/nodes", (req, res) => res.send(nodeList));
 
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
-
-
+app.listen(port, () =>
+  console.log(`Swarm visualiser listening on port ${port}`)
+);
