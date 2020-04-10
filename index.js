de8f62d2d37e7d5d6235dee667526638f83f28ac
Billy <billybofh@gmail.com>
2020-04-10 14:12:32 +0100

    Some tidying and comments

--- index.js
+++ index.js
@@ -3,10 +3,6 @@ const express = require("express");
 const cors = require("cors");
 const path = require("path");
 
-const app = express();
-app.use(cors());
-const docker = new Docker({ socketPath: "/var/run/docker.sock" });
-
 const port = process.env.VIS_PORT ? process.env.VIS_PORT : 3000;
 const refreshInterval = process.env.VIS_REFRESH
   ? process.env.VIS_REFRESH
@@ -14,6 +10,14 @@ const refreshInterval = process.env.VIS_REFRESH
 var nodeList = [];
 var taskList = [];
 
+// inistialise express
+const app = express();
+app.use(cors());
+
+// initialise the docker api client
+const docker = new Docker({ socketPath: "/var/run/docker.sock" });
+
+// trap any exit signals
 process.once("SIGINT", function(code) {
   console.log("SIGINT received...");
   process.exit(0);
@@ -23,16 +27,17 @@ process.once("SIGTERM", function(code) {
   process.exit(0);
 });
 
+// gets the current list of nodes and tasks from the docker api
 async function refreshDockerInfo() {
   nodeList = await docker.listNodes().catch((err) => console.error(err));
   taskList = await docker.listTasks().catch((err) => console.error(err));
   setTimeout(refreshDockerInfo, refreshInterval);
 }
 
-refreshDockerInfo();
-
+// set up express to serve any static files in the /public directory
 app.use(express.static("public"));
 
+// homepage
 app.get("/", (req, res) =>
   res.sendFile(
     "index.html",
@@ -42,9 +47,12 @@ app.get("/", (req, res) =>
     }
   )
 );
+// json endpoints for the swarm tasks and nodes
 app.get("/tasks", (req, res) => res.send(taskList));
 app.get("/nodes", (req, res) => res.send(nodeList));
 
+// and off we go
+refreshDockerInfo();
 app.listen(port, () =>
   console.log(`Swarm visualiser listening on port ${port}`)
 );
